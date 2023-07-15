c4cc26225042d8513de3022189c65450dfd3f24f
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2023-07-15 16:44:32 +0900

    .

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,15 +4,15 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
         if (isSignedIn) {
             return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p onClick={() => onRouteChange('signout')}className='f3 link dim black underline pa1 pointer'>Sign Out<br />ログアウト</p>
+            <p onClick={() => onRouteChange('signout')}className='f4 link dim black underline pa1 pointer'>Sign Out<br />ログアウト</p>
         </nav>
         );
         }
         else {
             return (
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p onClick={() => onRouteChange('signin')}className='f3 link dim black underline pa1 pointer'>Sign In<br />ログイン</p>
-            <p onClick={() => onRouteChange('register')}className='f3 link dim black underline pa1 pointer'>Register<br />登録</p>
+            <p onClick={() => onRouteChange('signin')}className='f4 link dim black underline pa1 pointer'>Sign In<br />ログイン</p>
+            <p onClick={() => onRouteChange('register')}className='f4 link dim black underline pa1 pointer'>Register<br />登録</p>
         </nav>
         );
         }
